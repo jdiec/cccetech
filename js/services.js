bf8b411d55cc523382ed3312dd1a4cf93415066f
jdiec <jpd276@cornell.edu>
2016-05-06 17:14:13 -0700

    comparisons section

--- js/services.js
+++ js/services.js
@@ -21,8 +21,8 @@ cccetechApp
     .service('comparisonFactory', [function(){
             var comparisons = [
                     {id:0, name:"Experience", img:"img/trello.jpg", description:"30 years of mechanical experience, overseas factory trained"},
-                    {id:1, name:"Affordability", img:"img/tech2.jpg", description:"Lower cost for higher quality compared to traditional services"},
-                    {id:2, name:"Flexibility", img:"img/tech3.jpg", description:"Willing to travel across the nation for on-site tech support"}
+                    {id:1, name:"Affordability", img:"img/tech.jpg", description:"Lower cost for higher quality compared to traditional services"},
+                    {id:2, name:"Flexibility", img:"img/tech4.png", description:"Willing to travel across the nation for on-site tech support"}
             ];
             this.getComparisons = function(){
                 return comparisons;    
